e2fcef89154b29263879a00823c7e378acdd0221
Lalitesh Tiwari <laliteshtiwari00@gmail.com>
2024-08-10 14:55:03 +0530

    auth env

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -4,8 +4,7 @@ const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYzE3YTEyZTExZjZhNzg4MmU5YmU5ZGIxNjNjZDJkYiIsIm5iZiI6MTcyMzE5OTY5NS4wOTk2Miwic3ViIjoiNjZiNWRjMTdjMTk4MGY2MzgzZDZkMjEzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.efFjJS_CVXloQ7hkLtdlIEuxfJ3tbRizxPpR5EVwcLc",
+    Authorization: import.meta.env.REACT_APP_AUTHKEY,
   },
 });
 
